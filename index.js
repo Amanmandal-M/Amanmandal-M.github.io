7bc076ce2ffa222202ce33a279832a84b16e6d1e
Aman Kumar <amanmandal644@gmail.com>
2023-02-13 23:07:39 +0530

    Letters Added

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
-document.querySelector("#resume").addEventListener("click", ()=>{
-    downloadAndViewFile("https://drive.google.com/file/d/1lyYfuQk3gqFDxPVYotKnOvk06lADgI8h/view");
-})
+document.querySelector("#resume").addEventListener("click", () => {
+  downloadAndViewFile(
+    "https://drive.google.com/file/d/1lyYfuQk3gqFDxPVYotKnOvk06lADgI8h/view"
+  );
+});
 function downloadAndViewFile(fileURL) {
-    const link = document.createElement("a");
-    link.href = fileURL;
-    link.download = "file.pdf";
-    link.target = "_blank";
-    link.click();
+  const link = document.createElement("a");
+  link.href = fileURL;
+  link.download = "file.pdf";
+  link.target = "_blank";
+  link.click();
 }
+
+//  LETTERS APPEND
+
+function append() {
+  var text = "I'm Aman Kumar";
+  var i = 0;
+  document.getElementById("user-detail-name").innerHTML=""
+  setInterval(() => {
+    if (text[i] == undefined) {
+      return;
+    }
+    document.getElementById("user-detail-name").innerHTML += text[i];
+    i++;
+  }, 150);
+}
+
+function append2() {
+    var text = "Node.js Backend Developer";
+    var i = 0;
+    document.getElementById("user-detail-intro").innerHTML=""
+    setInterval(() => {
+      if (text[i] == undefined) {
+        return;
+      }
+      document.getElementById("user-detail-intro").innerHTML += text[i];
+      i++;
+    }, 150);
+}
+
+append();
+append2();
+
+setInterval(()=>{
+    append();
+    append2();
+},7000)
+
