0f02a1a78571d9e3fac5c6c225cb0b0fa4c793b9
Aman Kumar <amanmandal644@gmail.com>
2023-04-12 03:20:49 +0530

    Resume link added

--- index.js
+++ index.js
@@ -7,20 +7,10 @@ function closemenu(){
     sidemenu.style.right="-200px";
 }
 
+  document.getElementById('resume-link-1').addEventListener("click",()=>{
+    window.location.assign("https://drive.google.com/file/d/1lyYfuQk3gqFDxPVYotKnOvk06lADgI8h/view", "_blank");
+  })
 
-document.querySelector("#resume-link-1").addEventListener("click", () => {
-  downloadAndViewFile(
-    "https://drive.google.com/file/d/1lyYfuQk3gqFDxPVYotKnOvk06lADgI8h/view"
-  );
-});
-function downloadAndViewFile(fileURL) {
-  const link = document.createElement("a");
-  link.href = fileURL;
-  link.target = "_blank";
-  link.click();
-}
-  
   document.getElementById('resume-link-2').addEventListener("click",()=>{
-    // console.log("OPENinig.....")
-    window.location.assign("https://drive.google.com/file/d/1lyYfuQk3gqFDxPVYotKnOvk06lADgI8h/view?usp=share_link", "_blank");
+    window.location.assign("https://drive.google.com/file/d/1lyYfuQk3gqFDxPVYotKnOvk06lADgI8h/view", "_blank");
   })
