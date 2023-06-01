d3563726c2e4ed68099cde085294c6ce258286e4
Amanmandal-M <amanmandal644@gmail.com>
2023-06-02 03:45:14 +0530

    Updated Portfolio

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function closemenu(){
     sidemenu.style.right="-200px";
 }
 
-  document.getElementById('.resume-link-1').addEventListener("click",()=>{
-    window.location.assign("https://drive.google.com/file/d/1lyYfuQk3gqFDxPVYotKnOvk06lADgI8h/view", "_blank");
+  document.getElementById('resume-link-1').addEventListener("click",()=>{
+    window.open("https://drive.google.com/file/d/1lyYfuQk3gqFDxPVYotKnOvk06lADgI8h/view", "_blank");
   })
 
   document.getElementById('resume-link-2').addEventListener("click",()=>{
-    window.location.assign("https://drive.google.com/file/d/1lyYfuQk3gqFDxPVYotKnOvk06lADgI8h/view", "_blank");
+    window.open("https://drive.google.com/file/d/1lyYfuQk3gqFDxPVYotKnOvk06lADgI8h/view", "_blank");
   })
