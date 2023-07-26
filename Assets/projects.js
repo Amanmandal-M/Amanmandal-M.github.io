acd30a1c613af85cb1258e3cc3bcfa37e46b9b82
Amanmandal-M <amanmandal644@gmail.com>
2023-07-27 01:11:16 +0530

    Upadating Project section

---
+++ Assets/projects.js
@@ -0,0 +1,347 @@
+// <!--------------------- Project Section --------------------->
+// <section class="projects section" id="projects">
+//   <h2 class="section-title">Latest Projects</h2>
+
+//   <div class="project-container">
+
+//     <!------- 9th Project ------->
+//     <div class="project-img project-card">
+//       <img
+//         id="project_WorkoutFitnessCenter"
+//         data-id="project_WorkoutFitnessCenter"
+//         src="./Assets/images/Projects/Object_Identifier/Object_Identifier.png"
+//         style="width: 100%"
+//         alt="Sixth Project"
+//       />
+//       <p class="project-title">Intelligent Object Classifier</p>
+//       <div class="project-subtitle project-description">
+//         Intelligent Object Classifier is a web application that uses machine learning to classify objects captured from a webcam in real-time. It leverages the power of TensorFlow.js and a pre-trained model to provide accurate object classification.
+//         <br />
+//         <br />
+//         Solo project: Completed in 1 day
+//         <br />
+//         <div class="project-tech-stack">
+//           <br />HTML | CSS | Javascript | NodeJS | ExpressJs | MongoDB | Machine Learning Model | TensorFlow.js
+//         </div>
+//       </div>
+//       <div class="project-btns">
+//         <a
+//           href="https://github.com/Amanmandal-M/Intelligent-Object-Classifier"
+//           target="_blank"
+//           class="small-btn button project-github-link"
+//         >
+//           Code&nbsp;&nbsp;<i class="bx bxl-github"></i>
+//         </a>
+//         <a
+//           href="https://object-identifier.netlify.app/"
+//           target="_blank"
+//           class="small-btn button project-deployed-link"
+//         >
+//           Live&nbsp;&nbsp;<i class="bx bxl-netlify"></i>
+//         </a>
+//       </div>
+//     </div>  
+
+//     <!------- 8th Project ------->
+//     <div class="project-img project-card">
+//       <img
+//         id="project_WorkoutFitnessCenter"
+//         data-id="project_WorkoutFitnessCenter"
+//         src="./Assets/images/Projects/Shayari_Generator/Shayari_Generator.png"
+//         style="width: 100%"
+//         alt="Sixth Project"
+//       />
+//       <p class="project-title">Shayari Generator</p>
+//       <div class="project-subtitle project-description">
+//         This is Shyaari Generator App so user can use this to generate shayaari in hindi or English Both.
+//         <br />
+//         <br />
+//         Solo project: Completed in 2 days
+//         <br />
+//         <div class="project-tech-stack">
+//           <br />HTML | CSS | Javascript | NodeJS | ExpressJs | MongoDB | OpenAI
+//         </div>
+//       </div>
+//       <div class="project-btns">
+//         <a
+//           href="https://github.com/Amanmandal-M/Shayari_Generator"
+//           target="_blank"
+//           class="small-btn button project-github-link"
+//         >
+//           Code&nbsp;&nbsp;<i class="bx bxl-github"></i>
+//         </a>
+//         <a
+//           href="https://shayari-generator.netlify.app/"
+//           target="_blank"
+//           class="small-btn button project-deployed-link"
+//         >
+//           Live&nbsp;&nbsp;<i class="bx bxl-netlify"></i>
+//         </a>
+//       </div>
+//     </div>           
+
+//     <!------- 7th Project ------->
+//     <div class="project-img project-card">
+//       <img
+//         id="project_WorkoutFitnessCenter"
+//         data-id="project_WorkoutFitnessCenter"
+//         src="./Assets/images/Projects/Code_Converter/Code_Converter.png"
+//         style="width: 100%"
+//         alt="Sixth Project"
+//       />
+//       <p class="project-title">Code Conversion Generator</p>
+//       <div class="project-subtitle project-description">
+//         This is Code conversion generator so developers or user they can convert their code in any languages.
+//         <br />
+//         <br />
+//         Solo project: Completed in 2 days
+//         <br />
+//         <div class="project-tech-stack">
+//           <br />HTML | CSS | Javascript | NodeJS | ExpressJs | MongoDB | OpenAI
+//         </div>
+//       </div>
+//       <div class="project-btns">
+//         <a
+//           href="https://github.com/Amanmandal-M/Code_Conversion_Generator"
+//           target="_blank"
+//           class="small-btn button project-github-link"
+//         >
+//           Code&nbsp;&nbsp;<i class="bx bxl-github"></i>
+//         </a>
+//         <a
+//           href="https://code-converter-genai.netlify.app/"
+//           target="_blank"
+//           class="small-btn button project-deployed-link"
+//         >
+//           Live&nbsp;&nbsp;<i class="bx bxl-netlify"></i>
+//         </a>
+//       </div>
+//     </div>        
+
+//     <!------- 6th Project ------->
+//     <div class="project-img project-card">
+//       <img
+//         id="project_WorkoutFitnessCenter"
+//         data-id="project_WorkoutFitnessCenter"
+//         src="https://user-images.githubusercontent.com/112626195/237268856-e8d835dc-e346-41f9-9e29-e9d448fc39d7.png"
+//         style="width: 100%"
+//         alt="Sixth Project"
+//       />
+//       <p class="project-title">Dental Booking Appointment System</p>
+//       <div class="project-subtitle project-description">
+//         This is a Dental Booking Appointment Booking System with Login, Sign up functionality and also having service page, doctors page, appointment booking page. 
+//         <br />
+//         <br />
+//         Collaborative project: Accomplished with a team of 5 developers,
+//         completed in 5 days
+//         <br />
+//         Contribution:- Manage Whole Backend | MVC | Schema | Documentation | Loggers | Admin | GitHub
+//         <div class="project-tech-stack">
+//           <br />HTML | CSS | Javascript | NodeJS | ExpressJs | MongoDB 
+//         </div>
+//       </div>
+//       <div class="project-btns">
+//         <a
+//           href="https://github.com/Amanmandal-M/Dental-Booking-Appointment-System"
+//           target="_blank"
+//           class="small-btn button project-github-link"
+//         >
+//           Code&nbsp;&nbsp;<i class="bx bxl-github"></i>
+//         </a>
+//         <a
+//           href="https://dentcare24x7.netlify.app/"
+//           target="_blank"
+//           class="small-btn button project-deployed-link"
+//         >
+//           Live&nbsp;&nbsp;<i class="bx bxl-netlify"></i>
+//         </a>
+//       </div>
+//     </div>
+
+//     <!------- 5th Project ------->
+//     <div class="project-img project-card">
+//       <img id="project_CloudTv"
+//         src="./Assets/images/Projects/HairSalon/E1.png"
+//         style="width: 100%"
+//         alt="Error" />
+//       <p class="project-title">Hair Salon Booking Appointment System</p>
+//       <div class="project-subtitle project-description">
+//         This is Hair Salon Booking Appointment System Backend here user can register with valid credentials and also they can login and view the stylers and services and book appointement by their choice.
+//         <br />
+//         <br />
+//         Collaborative project: Accomplished with a team of 5 developers,
+//         completed in 5 days
+//         <br />
+//         Contribution:- Manage Whole Backend | MVC | Admin | Documentation | Loggers | Nodemailer | Deployement
+//         <div class="project-tech-stack">
+//           <br />HTML | CSS | Javascript | NodeJS | ExpressJs | MongoDB 
+//         </div>
+//       </div>
+//       <div class="project-btns">
+//         <a
+//           href="https://github.com/Amanmandal-M/Hair-Salon-Booking-Appointment-System"
+//           target="_blank"
+//           class="small-btn button project-github-link"
+//         >
+//           Code&nbsp;&nbsp;<i class="bx bxl-github"></i>
+//         </a>
+//         <a
+//           href="https://snipsandspikes.netlify.app/"
+//           target="_blank"
+//           class="small-btn button project-deployed-link"
+//         >
+//           Live&nbsp;&nbsp;<i class="bx bxl-netlify"></i>
+//         </a>
+//       </div>
+//     </div>
+
+//     <!------- 4th Project ------->
+//     <div class="project-img project-card">
+//       <img
+//         id="project_TypeBattle"
+//         src="./Assets/images/Projects/FaceTime/D1.png"
+//         style="width: 100%"
+//         alt=""
+//       />
+//       <p class="project-title">Face Time Video Chat App</p>
+//       <div class="project-subtitle project-description">
+//         Face time chat app is a video chat project built using HTML, CSS, JavaScript, Express, Mongoose, Node.js, Socket.io, and WebRTC. The app allows users to communicate through chat messages and video calls in real-time.
+//         <br />
+//         <br />
+//         Collaborative project: Accomplished with a team of 5 developers,
+//         completed in 5 days
+//         <br />
+//         Contribution:- Landing Page | MVC Backend | WebRTC 
+//         <div class="project-tech-stack">
+//           <br />HTML | CSS | Javascript | NodeJS | MongoDB | Nodemailer | WebRTC | GitHub
+//         </div>
+//       </div>
+//       <div class="project-btns">
+//         <a
+//           href="https://github.com/Amanmandal-M/Face-Time-Chat-App"
+//           target="_blank"
+//           class="small-btn button project-github-link"
+//         >
+//           Code&nbsp;&nbsp;<i class="bx bxl-github"></i>
+//         </a>
+//         <a
+//           href="https://face-time-video.netlify.app/"
+//           target="_blank"
+//           class="small-btn button project-deployed-link"
+//         >
+//           Live&nbsp;&nbsp;<i class="bx bxl-netlify"></i>
+//         </a>
+//       </div>
+//     </div>
+
+//     <!------- 3rd Project ------->
+//     <div class="project-img project-card">
+//       <img
+//         id="project_OpenChat"
+//         src="./Assets/images/Projects/HindMart/C1.png"
+//         style="width: 100%"
+//         alt=""
+//       />
+//       <p class="project-title">Indiamart Clone</p>
+//       <div class="project-subtitle project-description">
+//         Hindmart is a clone of Indiamart. It is an Indian e-commerce website that provides B2C, B2B and customer to customer sales services via its web portal.<br />
+//         <br />
+//         Solo project: Completed in 5 days
+//         <br />
+//         <div class="project-tech-stack">
+//           <br />HTML | CSS | Javascript | NodeJS | ExpressJS | MongoDB | GitHub
+//         </div>
+//       </div>
+//       <div class="project-btns">
+//         <a
+//           href="https://github.com/Amanmandal-M/India-Mart-Clone"
+//           target="_blank"
+//           class="small-btn button project-github-link"
+//         >
+//           Code&nbsp;&nbsp;<i class="bx bxl-github"></i>
+//         </a>
+//         <a
+//           href="https://hind-bazaar.netlify.app/"
+//           target="_blank"
+//           class="small-btn button project-deployed-link"
+//         >
+//           Live&nbsp;&nbsp;<i class="bx bxl-netlify"></i>
+//         </a>
+//       </div>
+//     </div>
+
+//     <!------- 2nd Project ------->
+//     <div class="project-img project-card">
+//       <img
+//         id="project_GrabYourFit"
+//         src="./Assets/images/Projects/Sephora/B1.png"
+//         style="width: 100%"
+//         alt=""
+//       />
+//       <p class="project-title">Sephora-Clone</p>
+//       <div class="project-subtitle project-description">
+//         A Sephora consists of beauty make up products and where you can purchase the products related to Makeup things. <br />
+//         <br />
+//         Collaborative project: Accomplished with a team of 5 developers,
+//         completed in 5 days
+//         <br />
+//         Contribution:- Admin Page | Team lead
+//         <div class="project-tech-stack">
+//           <br />HTML | CSS | Javascript | GitHub
+//         </div>
+//       </div>
+//       <div class="project-btns">
+//         <a
+//           href="https://github.com/Amanmandal-M/Sephora-Clone"
+//           target="_blank"
+//           class="small-btn button project-github-link"
+//         >
+//           Code&nbsp;&nbsp;<i class="bx bxl-github"></i>
+//         </a>
+//         <a
+//           href="https://c4ra-store.netlify.app/"
+//           target="_blank"
+//           class="small-btn button project-deployed-link"
+//         >
+//           Live&nbsp;&nbsp;<i class="bx bxl-netlify"></i>
+//         </a>
+//       </div>
+//     </div>
+
+//     <!------- 1st Project ------->
+//     <div class="project-img project-card">
+//       <img
+//         id="project_PigGame"
+//         src="./Assets/images/Projects/SkinStore/E2.png"
+//         style="width: 100%"
+//         alt=""
+//       />
+//       <p class="project-title">SkinStore Clone</p>
+//       <div class="project-subtitle project-description">
+//         Skin Store is a Beauty Products website where user can register and login and view the beauty products and sort the products by price and use filter by the category of producs and buy their choice . 
+//         <br />
+//         <br />
+//         Solo project: Completed in 5 days
+//         <div class="project-tech-stack">
+//           <br />HTML | CSS | Javascript | Github
+//         </div>
+//       </div>
+//       <div class="project-btns">
+//         <a
+//           href="https://github.com/Amanmandal-M/Skin-Store-Clone"
+//           target="_blank"
+//           class="small-btn button project-github-link"
+//         >
+//           Code&nbsp;&nbsp;<i class="bx bxl-github"></i>
+//         </a>
+//         <a
+//           href="https://skin-tuner.netlify.app/"
+//           target="_blank"
+//           class="small-btn button project-deployed-link"
+//         >
+//           Live&nbsp;&nbsp;<i class="bx bxl-netlify"></i>
+//         </a>
+//       </div>
+//     </div>
+//   </div>
+// </section>
